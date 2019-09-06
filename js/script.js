c8c64d7d176d7bb8f04fbd8e9a42f7ee4858a2d5
Dmitry <example@gmail.com>
2019-09-06 13:54:17 +0300

    Доработка timer

--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
 	//Timer
 
-	let deadLine = '2019-10-7';
+	let deadLine = '2018-10-7';
 
 	function getTimeRemaining(endtime) {
 		let t = Date.parse(endtime) - Date.parse(new Date()),
@@ -64,13 +64,23 @@ window.addEventListener('DOMContentLoaded', function() {
 			timeInterval = setInterval(updateClock, 1000);
 
 		function updateClock() {
-			let t = getTimeRemaining(endtime);
-			hours.textContent = t.hours;
-			minutes.textContent = t.minutes;
-			seconds.textContent = t.seconds;
+            let t = getTimeRemaining(endtime);
+
+            function addZero(num){
+                        if(num <= 9) {
+                            return '0' + num;
+                        } else return num;
+                    };
+
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
 
 			if (t.total <= 0) {
 				clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
 			}
 		}	
 	}
